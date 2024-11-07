ba3c64fa8810b932f0b698b1ccb77d925c89cdb6
aiution <aiution.com@gmail.com>
2024-11-08 02:26:06 +0530

    dd

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,12 +10,12 @@ export default {
 		"2xl": "1536px",
 	  },
 	  colors: {
-		primary: "#B9FF66",
+		primary: "#cdbe78",
 		black: "#000000",
 		white: "#FFFFFF",
 		gray: "#0D0C22",
 		stone: "#231F20",
-		lime: "#B9FF66",
+		lime: "#cdbe78",
 		zinc: {
 		  100: "#F3F3F3",
 		  200: "#F0F0F0",
